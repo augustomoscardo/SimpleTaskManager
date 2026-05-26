5f76899faf0e3f0314bcce8bbf40cd3f2e4eda88
Augusto Moscardo <augustomsaluz@gmail.com>
2026-05-26 14:20:01 -0300

    Started Project

---
+++ SimpleTaskManager.API/Controllers/TaskController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleTaskManager.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TaskController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult GetAll()
+    {
+        return Ok();
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult GetById(Guid id)
+    {   
+        return Ok();
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public IActionResult Create()
+    {
+        return Ok();
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult Update(Guid id)
+    {
+        return NoContent();
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult Delete(Guid id)
+    {
+        return NoContent();
+    }
+}
