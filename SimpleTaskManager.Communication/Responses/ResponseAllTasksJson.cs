e7b79fe30cd0e9598e9fb42868ea52ab333693b6
Augusto Moscardo <augustomsaluz@gmail.com>
2026-05-28 09:13:53 -0300

    Add response DTOs.

---
+++ SimpleTaskManager.Communication/Responses/ResponseAllTasksJson.cs
@@ -0,0 +1,6 @@
+﻿namespace SimpleTaskManager.Communication.Responses;
+
+public class ResponseAllTasksJson
+{
+    public List<ResponseShortTaskJson> Tasks { get; set; } = [];
+}
---
+++ SimpleTaskManager.Communication/Responses/ResponseTaskJson.cs
@@ -0,0 +1,16 @@
+﻿using SimpleTaskManager.Communication.Enums;
+using TaskStatus = SimpleTaskManager.Communication.Enums.TaskStatus;
+
+namespace SimpleTaskManager.Communication.Responses;
+
+public class ResponseTaskJson
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public TaskPriority Priority { get; set; }
+    public DateOnly DueDate { get; set; }
+    public TaskStatus Status { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
